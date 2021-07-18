7c6576ee481b9c82c163df475eaeef4eef37118b
Fedor Livshits <fediliv@gmail.com>
2021-07-18 13:15:33 +0300

    rafactor menu list

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
-$(function(){
+$(function () {
+	$('.header-slider').slick({
+		arrows: false,
+		vertical: true,
+		dots: true,
+		dotsClass: 'header-dots',
+		autoplay: 2000,
+	})
 
-$('.header-slider').slick({
-    arrows: false,
-    vertical: true,
-    dots: true,
-    dotsClass: 'header-dots',
-    autoplay: 2000,
-});
-
-$('.menu__btn').on('click', function () {
-    $('.menu__list').slideToggle();
-}
-);
-});
+	$('.menu__btn').on('click', function () {
+		$('.menu__list').slideToggle()
+	})
+})
